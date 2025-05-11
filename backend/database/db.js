eb5688f2db4dc2fec1a00cecad492a9d72fb507b
Patel002 <patelvatsal79715@gmail.com>
2025-05-12 02:10:18 +0530

    Refactor database connection to use environment variables for configuration

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-    'u905741255_anishpharma0',
-    'u905741255_anishpharma0',
-    '9q3>pZEA*Vt',
+    process.env.DB_DATABASENAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
     {
-        host: '151.106.122.3',
+        host: process.env.DB_HOST,
         dialect: 'mysql',
         port: 3306
     }
