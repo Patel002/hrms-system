55ab0d219508f8c3d120fb464c0c6f8cf6d6fb9d
Patel002 <patelvatsal79715@gmail.com>
2025-06-30 12:06:23 +0530

    fix: Correct upload directory path for profile images in multer configuration

--- backend/middleware/multer.middleware.js
+++ backend/middleware/multer.middleware.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../uploads/profileImage/'); 
+    cb(null,'uploads','profileImage'); 
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + req.params.em_id + '_' + req.body.em_username;
