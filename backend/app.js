f7413d23f95b40d76fe7920595525f850a68a1e5
Patel002 <patelvatsal79715@gmail.com>
2025-06-30 11:17:38 +0530

    feat: Update upload path for profile images to ensure correct directory resolution

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,13 @@ app.use(cors());
 app.use(express.json({
     limit: '50mb'
 }));
+
 app.use(express.urlencoded({
     limit: '50mb',
     extended: true
 }));
 
-const uploadPath = path.resolve('uploads/profileImage');
+const uploadPath = path.resolve('../uploads/profileImage');
 if (!fs.existsSync(uploadPath)) {
     fs.mkdirSync(uploadPath),
     {
